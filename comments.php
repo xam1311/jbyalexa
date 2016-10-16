5d492d330864339ce3992b56458fdee861da35f4
xam1311 <github@designmyco.de>
2016-10-16 10:59:34 +0200

    Modification du form commentaire

--- comments.php
+++ comments.php
@@ -79,7 +79,38 @@ if ( post_password_required() ) {
 	<?php
 	endif;
 
-	comment_form();
-	?>
+
+$comment_args = array( 'title_reply'=>'Got Something To Say:',
+
+			'fields' => apply_filters( 'comment_form_default_fields', array(
+
+			'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Your Good Name' ) . '</label> ' . ( $req ? '<span>*</span>' : '' ) .
+
+			        '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"/></p>',   
+
+			    'email'  => '<p class="comment-form-email">' .
+
+			                '<label for="email">' . __( 'Your Email Please' ) . '</label> ' .
+
+			                ( $req ? '<span>*</span>' : '' ) .
+
+			                '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" />'.'</p>',
+
+			    'url'    => '' ) ),
+
+			    'comment_field' => '<p>' .
+
+			                '<label for="comment">' . __( 'Let us know what you have to say:' ) . '</label>' .
+
+			                '<textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>' .
+
+			                '</p>',
+
+			    'comment_notes_after' => '',
+
+      );
+
+comment_form($comment_args); ?>
+
 
 </div><!-- #comments -->
