9309e14ab5a126c9bf9825033d4217f04250cfc7
xam1311 <github@designmyco.de>
2017-02-05 22:15:38 +0100

    Modify for have the sidebar on mobile device

--- sidebar.php
+++ sidebar.php
@@ -9,7 +9,7 @@
 ?>
 <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 
-		<div class="col-xl-3 col-lg-3 col-md-4 hidden-sm-down" id="sidebar">
+		<div class="col-xl-3 col-lg-3 col-md-4 col-xs-12" id="sidebar">
 				<aside id="secondary" class="" role="complementary">
 					<?php dynamic_sidebar( 'sidebar-1' ); ?>
 				</aside><!-- #secondary -->
