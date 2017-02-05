25baf3da025fa701ea6439ac68224f7389c4c708
xam1311 <github@designmyco.de>
2017-02-05 22:10:18 +0100

    Correct forget imgSize type

--- archive.php
+++ archive.php
@@ -28,6 +28,8 @@ get_header(); ?>
 			<?php
 			/* Start the Loop */
 			$count = 0;
+			$imgSize = 'big';
+			/** on mets les tailles imgs à big **/
 			while ( have_posts() ) : the_post();
 			$count++;
 			set_query_var( 'count_loop', $count );
@@ -36,7 +38,9 @@ get_header(); ?>
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
+				 */	
+				set_query_var( 'count', $count );
+				set_query_var( 'img-size', $imgSize );
 				get_template_part( 'template-parts/content', get_post_format() );
 
 			endwhile; ?>
